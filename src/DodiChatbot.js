9f7626afc4e0d97a404ab804c4467b3ef1b3827f
czrvelasquez <108314800+czrvelasquez@users.noreply.github.com>
2024-11-05 14:15:50 -0600

    Update DodiChatbot.js

--- src/DodiChatbot.js
+++ src/DodiChatbot.js
@@ -297,7 +297,7 @@ const DodiChatbot = () => {
                     
         
                     if (requirePaymentProof) {
-                        addMessage("Dodi", "Gracias. Ahora, por favor sube tu comprobante de pago en formato PNG, JPG o PDF. (si seleccionaste planeación gratuita, omite este paso)");
+                        addMessage("Dodi", "Gracias. Ahora, por favor sube tu comprobante de pago en formato PNG, JPG o PDF. (solo archivos de Maximo 10 Mb)");
                         setMessages((prevMessages) => [
                             ...prevMessages,
                             {
